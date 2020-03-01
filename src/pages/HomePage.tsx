16b55d7b8e8efaeb4ae75df554dc4cb1ba8eb409
WarOnKhoff <voronkovs.aleksandrs@gmail.com>
2020-03-01 23:18:13 +0200

    Added typed text

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Footer from '../components/Footer'
 import Background from '../components/Backgroud'
+import { ThemeContext } from '../context/ThemeContext'
+import Typed from 'react-typed'
+import cx from 'clsx'
 
 const Home = () => {
+	const { theme } = useContext(ThemeContext)
+	const main = cx({
+		main: true,
+		main_ligthTheme: theme === 'light',
+	})
 	return (
-		<section className='main'>
+		<section className={main}>
 			<div className='main_group'>
-				<div className='main_group_item'>LeftSide</div>
-				<div className='main_group_item'>RightSide</div>
+				<div className='main_group_left'>
+					<h1 className='capture'>
+						<div>Simple loans</div>
+						<span>For Your </span>
+						<Typed
+							className='heroText'
+							strings={['IDEAS', 'DREAMS', 'PLANS', 'NEEDS', 'HOBBY']}
+							typeSpeed={100}
+							backSpeed={100}
+							backDelay={2000}
+							loop
+						/>
+					</h1>
+				</div>
+				<div className='main_group_right'>RightSide</div>
 			</div>
 			<Footer />
 			<Background />
